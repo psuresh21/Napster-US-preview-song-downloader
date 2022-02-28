c6992e62e806f5d6f67eb3d572e7f56e8974f3cc
psuresh21 <sureshpandiyanms@outlook.com>
2022-02-28 16:31:27 +0530

    napster

---
+++ napster.py
@@ -0,0 +1,20 @@
+from url_reads import url_reader
+from song_downloader import preview_download_napster
+import urllib.request
+import pprint
+
+
+
+print('welcome to preview song download form Napster')
+print('Copyright (c) by Suresh Pandiyan')
+print(' ')
+ask_music_url = str(input('enter the playlist for get a downloadable links \n'))
+print(' ')
+
+
+# read the napster urls
+url_reader(ask_music_url)
+
+# download song (preview only)
+preview_download_napster(url_reader(ask_music_url))
+
